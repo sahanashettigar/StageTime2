5548a8c8cf18e43c994b4bb3cb0c3da291353fd7
sahanashettigar <r.sahana.shettigar@gmail.com>
2021-02-27 18:43:55 +0530

    artist page and navbar

--- lib/screens/authenticate/profile.dart
+++ lib/screens/authenticate/profile.dart
@@ -4,15 +4,14 @@ class artistProfile extends StatefulWidget {
   @override
   _artistProfileState createState() => _artistProfileState();
 }
-Color myColor = Color(0xff01395E);
 
+Color myColor = Color(0xff01395E);
 
 class _artistProfileState extends State<artistProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: myColor,
-      
     );
   }
 }
